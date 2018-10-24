bf5a5d77f509fdbc69d0da396eaa91681ab6cc6b
Dakota <dakota.vradenburg@gmail.com>
2018-10-24 11:27:33 -0400

    Added section to main page for friends. Added js for adding information to friends.

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ console.log("Hello Kool Kidz!");
 $(document)
 .on("click", "button", () => {
     console.log("You clicked me!");
+    $(".friends").append(`
+    <p>First name: ${$("input").eq(0).val()}</p>
+    <p>Last name: ${$("input").eq(1).val()}</p>
+    <p>Favorite food: ${$("input").eq(2).val()}</p>
+    `);
 })
 .on("mouseenter", "button", (event) => {
     $(event.target)
@@ -18,5 +23,4 @@ $(document)
         .css("background-color", "darkmagenta")
         .css("color", "white");
 })
-
 });
