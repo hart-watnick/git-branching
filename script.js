a41ada7ee96cbeded230caa7f1bb9719218af752
Dakota <dakota.vradenburg@gmail.com>
2018-10-24 10:38:17 -0400

    Added mouse events and changed console message.

--- script.js
+++ script.js
@@ -2,10 +2,21 @@
 
 $(document).ready(() => {
 
-console.log("Hello!");
+console.log("Hello Kool Kidz!");
 
-$(document).on("click", "button", () => {
+$(document)
+.on("click", "button", () => {
     console.log("You clicked me!");
-});
+})
+.on("mouseenter", "button", (event) => {
+    $(event.target)
+        .css("background-color", "pink")
+        .css("color", "darkred");
+})
+.on("mouseleave", "button", (event) => {
+    $(event.target)
+        .css("background-color", "darkmagenta")
+        .css("color", "white");
+})
 
 });
