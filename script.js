9d3715442a686685dcedddd0931c4109b1681c13
Dakota <dakota.vradenburg@gmail.com>
2018-10-24 11:53:23 -0400

    Added cool styling to our friends list.

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 
 $(document).ready(() => {
 
-console.log("Hello Kool Kidz!");
+    console.log("Hello Kool Kidz!");
 
-$(document)
-.on("click", "button", () => {
-    console.log("You clicked me!");
-    $(".friend-container").append(`
-    <p>First name: ${$("input").eq(0).val()}</p>
-    <p>Last name: ${$("input").eq(1).val()}</p>
+    $(document)
+        .on("click", "button", () => {
+            if ($("input").eq(0).val() && $("input").eq(1).val() && $("input").eq(2).val()) {
+                $(".friend-container").append(`
+    <div class="friend">
+    <p class="name">${$("input").eq(0).val()} ${$("input").eq(1).val()}</p>
     <p>Favorite food: ${$("input").eq(2).val()}</p>
+    </div>
     `);
-})
-.on("mouseenter", "button", (event) => {
-    $(event.target)
-        .css("background-color", "pink")
-        .css("color", "darkred");
-})
-.on("mouseleave", "button", (event) => {
-    $(event.target)
-        .css("background-color", "darkmagenta")
-        .css("color", "white");
-})
+            }
+        })
+        .on("mouseenter", "button", (event) => {
+            $(event.target)
+                .css("background-color", "orange")
+                .css("color", "black")
+                .css("font-weight", "bold")
+        })
+        .on("mouseleave", "button", (event) => {
+            $(event.target)
+                .css("background-color", "darkmagenta")
+                .css("color", "white");
+        })
 });
